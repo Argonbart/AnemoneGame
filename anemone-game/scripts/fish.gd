ecc6e7bc7aeb576438186cb0fd226d4933f6f3cd
Alex <104043084+Argonbart@users.noreply.github.com>
2025-11-08 17:24:29 +0100

    WIP: Add new fish version (closed worm, can still collapse on itself)

--- anemone-game/scripts/fish.gd
+++ anemone-game/scripts/fish.gd
@@ -3,106 +3,93 @@ extends Node2D
 
 
 const FISH_LINK = preload("uid://b8kv0npx7hj7a")
+const FISH_LENGTH: int = 5
+const FISH_LINK_RADIUS: float = 20.0
 
-@export_tool_button("Generate fish form") var generate_fish_form_button = generate_fish_form
+@export_tool_button("Generate fish form") var generate_fish_form_button = generate_fish
 
 @export_category("Components")
 @export var fish_outline: Line2D
 
-var _link_sizes = []
 var _fish_links: Array[FishLink]
 
 
 func _ready():
-	generate_fish_form()
+	generate_fish()
 
 
-func _process(delta):
+func generate_fish():
 	
-	draw_outline()
-	
-	if Engine.is_editor_hint():
-		return
-	
-	var head = get_child(get_children().size() - 1)
-	var snd_link = get_child(get_children().size() - 2)
-	var trd_link = get_child(get_children().size() - 3)
-	var speed = 500.0
-	var turn_factor = 0.5
-	
-	var moving_dir: Vector2 = (head.position - snd_link.position).normalized()
-	var moving_dir_1 = moving_dir
-	var moving_dir_2: Vector2 = (snd_link.position - trd_link.position).normalized()
-	if Input.is_action_pressed("left"):
-		moving_dir = (moving_dir.rotated((-PI / 2.0) * turn_factor)).normalized()
-	if Input.is_action_pressed("right"):
-		moving_dir = (moving_dir.rotated((PI / 2.0) * turn_factor)).normalized()
-	var turn_angle = moving_dir_1.angle_to(moving_dir_2)
-	var angle_threshold = 0.35
-	if abs(turn_angle) > angle_threshold:
-		turn_angle = angle_threshold
-		moving_dir = moving_dir_1.normalized()
-	head.position += moving_dir * speed * delta
-
-
-func generate_fish_form():
-	var new_number_of_fish_links = randi_range(8, 8)
-	var new_link_sizes = []
-	#new_link_sizes.append(randf_range(20.0, 20.0))
-	#new_link_sizes.append(randf_range(25.0, 25.0))
-	#new_link_sizes.append(randf_range(20.0, 20.0))
-	for link_index in range(new_number_of_fish_links):
-		new_link_sizes.append(randf_range(40.0, 40.0))
-	_link_sizes = new_link_sizes
-	draw_fish()
-
-
-func draw_fish():
-	# reset
+	_fish_links.clear()
 	for child in get_children():
 		if child.is_in_group("fish_link"):
 			child.free()
-	var fish_length = 0.0
-	_fish_links = []
 	
-	var i = 1
-	for link_length in _link_sizes:
+	var fish_length = 0.0
+	for link_index in range(FISH_LENGTH):
 		var new_link: FishLink = FISH_LINK.instantiate()
-		if not _fish_links.is_empty():
-			var last_link = _fish_links[_fish_links.size() - 1]
-			last_link.next_link = new_link
-			new_link.prev_link = last_link
-		_fish_links.append(new_link)
-		new_link.name = str("FishLink", i)
 		new_link.add_to_group("fish_link")
-		i += 1
-		new_link.radius = link_length
+		new_link.radius = FISH_LINK_RADIUS
 		new_link.position = Vector2(fish_length, 0.0)
 		fish_length = fish_length + new_link.radius
 		self.add_child(new_link)
 		new_link.owner = self
-		draw_outline()
+		if link_index == 0:
+			new_link.is_tail = true
+			# SET new_link.tail_points
+		if link_index == FISH_LENGTH - 1:
+			new_link.is_head = true
+			# SET new_link.head_points
+		_fish_links.append(new_link)
+	for link_index in range(_fish_links.size()):
+		var link = _fish_links[link_index]
+		if not link.is_head:
+			link.next = _fish_links[link_index + 1]
+	draw_outline()
 
+
+func _process(delta):
+	
+	var head: FishLink = _fish_links[_fish_links.size() - 1]
+	var tail: FishLink = _fish_links[0]
+	var targetPos = get_viewport().get_mouse_position()
+	var headPos = head.global_position
+	var headDir = (targetPos - headPos).normalized()
+	
+	# TODO: Add turning constraint
+	
+	headPos += headDir * delta * 400.0
+	if (targetPos - headPos).length() > 5.0:
+		head.update_head(headPos, headDir)
+		tail.update_tail()
+	
+	draw_outline()
 
 func draw_outline():
-	var outline_points = []
-	if (_fish_links.size() - 1) * 2 < 0.0:
-		return
-	outline_points.resize((_fish_links.size() - 1) * 2)
-	outline_points.fill(0.0)
 	
-	for link_index in range(_fish_links.size() - 1):
-		if link_index == _fish_links.size() - 1:
-			#for point in _fish_links[link_index + 1]._points:
-				#outline_points.insert(link_index, point + _fish_links[link_index + 1].position)
+	var outline_points = []
+	var left_points = []
+	var right_points = []
+	var head_points = []
+	var tail_points = []
+	
+	for link in _fish_links:
+		if link == _fish_links[0]:
+			tail_points = link.tail_points
 			continue
-		var next_link_index = link_index + 1
-		var vector_to_next_link = _fish_links[next_link_index].position - _fish_links[link_index].position
-		var vector_middle_to_side_left = vector_to_next_link.rotated(PI / 2.0)
-		var vector_middle_to_side_right = vector_to_next_link.rotated(-PI / 2.0)
-		var vector_to_side_point_left = _fish_links[link_index].position + vector_middle_to_side_left
-		var vector_to_side_point_right = _fish_links[link_index].position + vector_middle_to_side_right
-		outline_points[link_index] = vector_to_side_point_left
-		outline_points[(_fish_links.size() * 2) - 3 - link_index] = vector_to_side_point_right
+		if link == _fish_links[_fish_links.size() - 1]:
+			head_points = link.head_points
+		left_points.append(link.left_point)
+		right_points.append(link.right_point)
+	
+	for point in left_points:
+		outline_points.append(point)
+	for point in head_points:
+		outline_points.append(point)
+	right_points.reverse()
+	for point in right_points:
+		outline_points.append(point)
+	for point in tail_points:
+		outline_points.append(point)
 	
 	fish_outline.points = outline_points
--- anemone-game/scripts/fish_link.gd
+++ anemone-game/scripts/fish_link.gd
@@ -7,57 +7,67 @@ extends Node2D
 @export_range(0.0, 100.0, 1.0) var radius: float = 50.0:
 	set(value):
 		radius = value
-@export_range(0.0, 100.0, 1.0) var number_of_points: float = 10.0:
-	set(value):
-		number_of_points = value
-@export var fill_color: Color:
-	set(value):
-		fill_color = value
-@export var stroke_color: Color:
-	set(value):
-		stroke_color = value
-@export_range(0.0, 100.0, 1.0) var stroke_width: float = 10.0:
-	set(value):
-		stroke_width = value
-@export_tool_button("Redraw") var redraw_button = draw_link
 
-@export_category("Components")
-@export var polygon_2d: Polygon2D
-@export var line_2d: Line2D
+# Link data
+var link_dir: Vector2 = Vector2.ZERO
+var left_point: Vector2 = Vector2.ZERO
+var right_point: Vector2 = Vector2.ZERO
 
-var _points: Array[Vector2] = []
-var prev_link: FishLink = null
-var next_link: FishLink = null
+# Head data
+var is_head: bool = false
+var head_points = []
+
+# Tail data
+var is_tail: bool = false
+var tail_points = []
+
+# Body data
+var next: FishLink = null
 
 
-func _ready():
-	draw_link()
-
-
+# Updates link if not head
 func _process(delta):
-	var speed = 400.0
-	if prev_link:
-		var distance_to_prev_link = abs(prev_link.position - self.position).length()
-		var dir_to_prev_link = (prev_link.position - self.position).normalized()
-		if distance_to_prev_link > 40.0:
-			self.position += dir_to_prev_link * speed * delta
-	if next_link:
-		var distance_to_next_link = abs(next_link.position - self.position).length()
-		var dir_to_next_link = (next_link.position - self.position).normalized()
-		if distance_to_next_link > 40.0:
-			self.position += dir_to_next_link * speed * delta
+	
+	#if Engine.is_editor_hint():
+		#return
+	
+	if not is_head and next:
+		var new_pos = self.position
+		var new_dir = (next.position - self.position).normalized()
+		var next_distance = abs(next.position - self.position).length()
+		if next_distance > 20.0:
+			new_pos += new_dir * 400.0 * delta
+		update_link(new_pos, new_dir)
 
 
-func draw_link():	
-	_points.clear()
-	for point_index in range(number_of_points):
-		var point_angle = (2 * PI / number_of_points) * point_index
-		var point_x = radius * cos(point_angle)
-		var point_y = radius * sin(point_angle)
-		var point_pos = Vector2(point_x, point_y)
-		_points.append(point_pos)
-	polygon_2d.polygon = _points
-	polygon_2d.color = fill_color
-	line_2d.points = _points
-	line_2d.default_color = stroke_color
-	line_2d.width = stroke_width
+# Updates link position, direction and side points
+func update_link(pos: Vector2, dir: Vector2):
+	global_position = pos
+	link_dir = dir
+	left_point = pos + dir.rotated(PI/2.0) * radius
+	right_point = pos + dir.rotated(-PI/2.0) * radius
+
+
+func update_head(pos: Vector2, dir: Vector2):
+	global_position = pos
+	link_dir = dir
+	left_point = pos + dir.rotated(PI/2.0) * radius
+	right_point = pos + dir.rotated(-PI/2.0) * radius
+	
+	head_points.clear()
+	for i in range(4):
+		head_points.append(pos + dir.rotated(((PI/2.0) / 5.0) * (4-i)) * radius)
+	head_points.append(pos + dir * radius)
+	for i in range(4):
+		head_points.append(pos + dir.rotated(((-PI/2.0) / 5.0) * (i+1)) * radius)
+
+
+func update_tail():
+	var pos = global_position
+	var dir = -link_dir
+	tail_points.clear()
+	for i in range(4):
+		tail_points.append(pos + dir.rotated(((PI/2.0) / 5.0) * (4-i)) * radius)
+	tail_points.append(pos + dir * radius)
+	for i in range(4):
+		tail_points.append(pos + dir.rotated(((-PI/2.0) / 5.0) * (i+1)) * radius)
