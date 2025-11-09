8c586c914ab9d3727025170a49f370a3fa133bc7
Tobias Heiles <contact@tobiasheiles.de>
2025-11-09 16:42:42 +0100

    Add start menu

--- anemone-game/scripts/game_state/game_config.gd
+++ anemone-game/scripts/game_state/game_config.gd
@@ -6,10 +6,10 @@ var map_half_size_x := map_size_x / 2.0
 var map_half_size_y := map_size_y / 2.0
 var fish_nav_screen_margin := 500.0
 
-var phase_0_max_trash := 20
-var phase_1_max_trash := 15
-var phase_2_max_trash := 10
-var phase_3_max_trash := 5
+var phase_0_max_trash := 4
+var phase_1_max_trash := 3
+var phase_2_max_trash := 2
+var phase_3_max_trash := 1
 
 var trash_spawn_screen_marging := 3500.0
 var trash_spawn_limit_x_left := -map_half_size_x + trash_spawn_screen_marging
