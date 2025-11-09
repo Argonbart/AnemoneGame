885f72083c42c6656710e10aa05a690f06756a98
Leo Schurrer <l.schurrer@oth-aw.de>
2025-11-09 16:52:54 +0100

    Ambient music

--- anemone-game/scripts/cutscenes/cutscene_manager.gd
+++ anemone-game/scripts/cutscenes/cutscene_manager.gd
@@ -19,7 +19,6 @@ extends Node2D
 
 var active_scene : Node2D = null
 
-
 func _ready() -> void:
 	SignalBus.begin_cutscene.connect(on_begin_cutscene)
 	SignalBus.end_cutscene.connect(on_end_cutscene)
@@ -31,10 +30,15 @@ func on_begin_cutscene(scene_id: int):
 	
 	if scene_id == 1:
 		active_scene = phase_1_scene.instantiate()
+		AudioManager.play("sad")
 	elif scene_id == 2:
 		active_scene = phase_2_scene.instantiate()
+		AudioManager.stop_all_of_type(Sound.AudioType.MUSIC)
+		AudioManager.play("middle")
 	elif scene_id == 3:
 		active_scene = phase_3_scene.instantiate()
+		AudioManager.stop_all_of_type(Sound.AudioType.MUSIC)
+		AudioManager.play("happy")
 	elif scene_id == 4:
 		active_scene = win_scene.instantiate()
 	elif scene_id == 5:
