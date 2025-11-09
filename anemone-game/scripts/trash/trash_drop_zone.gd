c7220317f9f60f2a647d14c87da6fd5454cf663c
Leo Schurrer <l.schurrer@oth-aw.de>
2025-11-09 16:37:52 +0100

    Trash sfx

--- anemone-game/scripts/trash/trash_drop_zone.gd
+++ anemone-game/scripts/trash/trash_drop_zone.gd
@@ -4,3 +4,9 @@ extends Area2D
 func _on_body_entered(_body: Node2D) -> void:
 	print('drop zone: ' + str(_body))
 	SignalBus.trash_dropped.emit()
+	if randf() < 0.333333:
+		AudioManager.play("sfx_suck_in")
+	elif randf() < 0.33333:
+		AudioManager.play("sfx_suck_in_1")
+	else:
+		AudioManager.play("sfx_suck_in_2")
